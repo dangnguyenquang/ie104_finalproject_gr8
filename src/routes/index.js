d5d018aea9b0436be4d5bff11dc30216fb37b86e
Nguyễn Bá <121294992+nguyenba16@users.noreply.github.com>
2024-10-27 20:19:35 +0700

    feat: initial version Menu page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,10 @@ import AboutUs from '~/pages/AboutUs'
 import Menu from '~/pages/Menu'
 import SellerRegisterForm from '~/pages/SellerRegisterForm'
 import { routes } from '~/configs'
-import { patch } from '@mui/material'
 
 const publicRoutes = [
   { path: routes.HOME, component: Home },
+  { path: routes.MENU, component: Menu },
   { path: routes.ABOUTUS, component: AboutUs },
   { path: routes.SELLER_REGISTER, component: SellerRegisterForm },
 ]
