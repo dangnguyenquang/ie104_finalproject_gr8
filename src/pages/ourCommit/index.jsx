4b71d4a6215fd6097a2c0e012825872a46af0043
NguyenQuangDang <dangnguyenquangit@gmail.com>
2024-12-16 10:36:32 +0700

    feat: update protected wrapper

--- src/pages/ourCommit/index.jsx
+++ src/pages/ourCommit/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export default function OurCommit() {
+export default OurCommit = () => {
   return (
     <div className='py-20 flex justify-center items-center'>
       <div>
